42a8c80c835659440ff83976d78f35c041f834e4
Marcus Turnbo <mturnbo@gogoair.com>
2017-01-27 16:22:27 -0600

    Added eslint and updated webpack config

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
+const webpackConfig = {
+  entry: {
+    app: ['./app/initialize']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+      }
+    ]
+  },
+  output: {
+    filename: 'app.js',
+    path: path.join(__dirname, './public'),
+    publicPath: '/public/'
+  },
+  plugins: [
+    new ExtractTextPlugin('app.css'),
+    new CopyWebpackPlugin([{
+      from: './app/assets/index.html',
+      to: './index.html'
+    }])
+  ],
+  resolve: {
+    root: path.join(__dirname, './app')
+  },
+  resolveLoader: {
+    root: path.join(__dirname, './node_modules')
+  }
+}
